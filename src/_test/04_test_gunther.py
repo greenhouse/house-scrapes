86a544de0b78b60d92a6e8e7d9bcf4e4e42a99a3
house <housesellout@gmail.com>
2023-06-16 15:35:26 -0400

    … added new lead test .py templates (abraham, kuna, gunther, silk) 	- just blank templates 	- these leads have all responded with details 		and maybe expecting demos 		except ‘silk’, no response at this point, but have details to start

---
+++ src/_test/04_test_gunther.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+__filename = 'test_gunther.py'
+__fname = 'test_gunther'
+cStrDividerExcept = '***************************************************************'
+cStrDivider = '#================================================================#'
+print('', cStrDivider, f'START _ {__filename}', cStrDivider, sep='\n')
+print(f'GO {__filename} -> starting IMPORTs and globals decleration')
+
+#------------------------------------------------------------#
+#   IMPORTS                                                  #
+#------------------------------------------------------------#
+from datetime import datetime
+
+#------------------------------------------------------------#
+#   PROCEDURAL SUPPORT                                       #
+#------------------------------------------------------------#
+
+
+#------------------------------------------------------------#
+#   DEFAULT SUPPORT                                          #
+#------------------------------------------------------------#
+#def go_main():
+#    run_time_start = get_time_now()
+#    print(f'\n\nRUN_TIME_START: {run_time_start}')
+#    read_cli_args() # print cli args
+#    argCnt = len(sys.argv) # get arg cnt
+#
+#    # validate args
+#    if argCnt > 1:
+#        print('*** ERROR *** _ invalid args\n ... exiting\n\n')
+#        exit(1)
+#
+#    # loop through and scrape each url
+#    exe_pg_scrape_loop(LST_PG_URLS, WAIT_TIME)
+#
+#    print(f'\n\nRUN_TIME_START: {run_time_start}')
+#    print(f'RUN_TIME_END:   {get_time_now()}')
+#
+#def read_cli_args():
+#    funcname = f'<{__filename}> _ ENTER _ read_cli_args'
+#    print(f'\n{funcname}...')
+#    argCnt = len(sys.argv)
+#    print(' # of args: %i' % argCnt)
+#    print(' argv lst: %s' % str(sys.argv))
+#    for idx, val in enumerate(sys.argv):
+#        print(f' argv[{idx}]: {val}')
+#    print(f'DONE _  read_cli_args...')
+#
+#if __name__ == "__main__":
+#    go_main()
+
+
+
