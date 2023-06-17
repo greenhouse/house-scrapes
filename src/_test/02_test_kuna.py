2a89aebd86ab9ca0f20606f4b4e7cd4910a52f6c
house <housesellout@gmail.com>
2023-06-17 17:10:20 -0400

    :- KUNA: finalized demo integration (ready for screen recording) 	- finished support for scraping list of 4 test urls 		coming from ‘?page=1’ of yourstory.com test site (provided by kuna) 	- decouple web driver initialize from ‘scrape_target_pg’ 		- added ‘init_webdriver’ 	- added support for randomizing wait_sleep time 		attempting to avoid bot detection (randomize timing of requests) 	- added support for calling test html file (pre-extracted) 		attempting to avoid bot detection (minimizing net requests) 		added ’02_test_kuna_html_1.py’ 		added support for importing .py files name that start with a number 			‘import importlib’ 	- misc cleanup
    	NOTE:
    		ready to create / move over to …/src/kuna/kuna_ystory_scrape.py
    		ready to create demo screen recording and send to kuna

--- src/_test/02_test_kuna.py
+++ src/_test/02_test_kuna.py
@@ -24,60 +24,81 @@ from selenium.webdriver.chrome.options import Options
     #from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
     #from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
     #from selenium.webdriver.firefox.options import Options
+import random
+import importlib
+HTML_1 = importlib.import_module('02_test_kuna_html_1')
 
 #------------------------------------------------------------#
 #   GLOBALS                                                  #
 #------------------------------------------------------------#
-WAIT_TIME = 30 # sec
+WAIT_TIME = 20 # sec
 #WAIT_TIME = 60 * 60 # 1 hr
 
 LST_PG_URLS = [ # GET https://yourstory.com/search?page=1&category=Funding
     # NOTE: '?page=0' returns empty lst of results (no err pg|msg)
     "https://yourstory.com/2023/06/weekly-funding-roundup-june-12-16-vc-investments-rise-lenskart",
-    "https://yourstory.com/2023/06/lenskart-raises-100-million-chryscapital-private-equity"
+    "https://yourstory.com/2023/06/lenskart-raises-100-million-chryscapital-private-equity",
+    "https://yourstory.com/2023/06/bengaluru-based-home-interior-startup-homelane",
+    "https://yourstory.com/2023/06/digital-health-startup-mykare-health-raises-seed-round"
 ]
 
 #------------------------------------------------------------#
 #   PROCEDURAL SUPPORT                                       #
 #------------------------------------------------------------#
-def scrape_target_pg(page_url : str):
+def scrape_target_pg(driver, page_url : str):
+    req_time_start = datetime.now().strftime("%H:%M:%S.%f")[0:-4]
+    
+    print('\nGetting page_url content...')
+    driver.get(page_url)
+    html_content = driver.page_source
+    #html_content = HTML_1.TEST_HTML # from 02_test_kuna_html_1.py
+
+    # parse out article descr & body text
+    print('\nParsing "description" & "body"...')
+    idx_start = html_content.index('"description":"')+len('"description":"')
+    idx_end = html_content[idx_start:].index('"')
+    descr = html_content[idx_start:idx_start+idx_end+1]
+    print('\nDESCR:\n '+descr)
+    
+    idx_start = html_content.index('"articleBody":"')+len('"articleBody":"')
+    idx_end = html_content[idx_start:].index('"')
+    body = html_content[idx_start:idx_start+idx_end+1]
+    print('\nBODY:\n '+body)
+
+    # print OG html version
+    #print(f"\n\n _ html_content (OG) _ \n{html_content}")
+    
+    # translate html_content from french to english (fr -> en)
+    #translator = Translator(service_urls=['translate.google.com'])
+    #html_content_trans = translator.translate(html_content, src='fr', dest='en').text
+
+    # print TRANS html version
+    #print(f"\n\n _ html_content (TRANS) _ \n"+html_content_trans)
+
+    #print(f'\n\n scraping {page_url} _ DONE _')
+
+def init_webdriver():
     ## Selenium: init webdrive ##
-    print(f'\nInitializing Selenium webdrive...')
+    print(f'\nInitializing Selenium webdriver...')
 
     # Configure Selenium options
     options = Options()
     options.add_argument("--headless")  # Run Chrome in headless mode
 
-    # Create a new Selenium driver
-    #driver = webdriver.Firefox(options=options) # eg_051723: firefox taking 30sec+
-    driver = webdriver.Chrome(options=options)
-
-    ## Selenium: retreive html_content ##
-    req_time_start = datetime.now().strftime("%H:%M:%S.%f")[0:-4]
-    driver.get(page_url)
-    html_content = driver.page_source
-
-    # print OG html version
-    #print(f"\n\n _ html_content (OG) _ \n{html_content}")
+    # Create a new Selenium driver & get html_content
+    return webdriver.Chrome(options=options)
     
-#    # translate html_content from french to english (fr -> en)
-#    translator = Translator(service_urls=['translate.google.com'])
-#    html_content_trans = translator.translate(html_content, src='fr', dest='en').text
-#
-#    # print TRANS html version
-#    print(f"\n\n _ html_content (TRANS) _ \n"+html_content_trans)
-
-    print(f'\n\n scraping {page_url} _ DONE _')
-
-def exe_pg_scrape_loop(lst_pgs, wait_sec : float):
+def exe_pg_scrape_loop(lst_pgs: list, wait_sec : float):
+    driver = init_webdriver()
     for pg_url in lst_pgs:
         go_time_start = get_time_now()
-        print(f'pg scrape start: {go_time_start}')
-        scrape_target_pg(pg_url)
-        print(f'pg scrape start: {go_time_start}\npg scrape end:   {get_time_now()}')
+        print(f'\n pg scrape start: {go_time_start}\n    url: {pg_url}')
+        scrape_target_pg(driver, pg_url)
+        print(f'\n pg scrape start: {go_time_start}\n pg scrape end:   {get_time_now()}\n    url: {pg_url}\n\n')
             
         # sleep 'wait_sec' before next url
-        wait_sleep(wait_sec)
+        r_sec = int(random.uniform(wait_sec-10, wait_sec+5))
+        wait_sleep(r_sec)
 
 #------------------------------------------------------------#
 #   DEFAULT SUPPORT                                          #
@@ -96,7 +117,7 @@ def go_main():
     exe_pg_scrape_loop(LST_PG_URLS, WAIT_TIME)
     print(f'\n\nRUN_TIME_START: {run_time_start}\nRUN_TIME_END:   {get_time_now()}')
 
-def wait_sleep(wait_sec, b_print=True): # sleep 'wait_sec'
+def wait_sleep(wait_sec : int, b_print=True): # sleep 'wait_sec'
     for s in range(wait_sec, 0, -1):
         if b_print: print('wait ', s, sep='', end='\n')
         time.sleep(1)
@@ -109,15 +130,6 @@ def read_cli_args():
     for idx, val in enumerate(sys.argv): print(f' argv[{idx}]: {val}')
     print('read_cli_args _ DONE\n')
     return sys.argv
-    
-#    funcname = f'<{__filename}> _ ENTER _ read_cli_args'
-#    print(f'\n{funcname}...')
-#    argCnt = len(sys.argv)
-#    print(' # of args: %i' % argCnt)
-#    print(' argv lst: %s' % str(sys.argv))
-#    for idx, val in enumerate(sys.argv):
-#        print(f' argv[{idx}]: {val}')
-#    print(f'DONE _  read_cli_args...')
 
 if __name__ == "__main__":
     go_main()
