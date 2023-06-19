cfd1eaa9eea1f3c54f733740c500bcf594e4d803
house <housesellout@gmail.com>
2023-06-18 22:40:26 -0400

    :- GUN: test .py (formatted initial code base template) 	- set up default imports and globals 	- set up default function scope and call stack 		scrape_target_pg 		init_webdriver 		exe_pg_scrape_loop 		go_main 		wait_sleep 		get_time_now 		read_cli_args 		if __name__ == "__main__": 	- added 04_test_gun_html_1.py (blank template) 	- ready for algorithmic integration

--- src/_test/04_test_gun.py
+++ src/_test/04_test_gun.py
@@ -9,45 +9,125 @@ print(f'GO {__filename} -> starting IMPORTs and globals decleration')
 #------------------------------------------------------------#
 #   IMPORTS                                                  #
 #------------------------------------------------------------#
+import sys, argparse, string, ctypes, os, re
+import json
+import time
 from datetime import datetime
+from lxml import html
+from bs4 import BeautifulSoup
+from selenium import webdriver # pip install -U selenium
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+    #from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+    #from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+    #from selenium.webdriver.firefox.options import Options
+import random
+import importlib
+HTML_1 = importlib.import_module('04_test_abe_html_1')
+
+#------------------------------------------------------------#
+#   GLOBALS                                                  #
+#------------------------------------------------------------#
+WAIT_TIME = 10 # sec
+WR_HI = 0
+WR_LOW = -5
+AUTO_CLICK_WAIT = True
+LOCAL_TEST = False
+LST_PG_URLS = [ # GET https://news.mongabay.com/?s=illegal+logging
+    "https://news.mongabay.com/2023/03/indonesian-campaigns-getting-money-from-illegal-logging-mining-watchdog-says/"
+]
 
 #------------------------------------------------------------#
 #   PROCEDURAL SUPPORT                                       #
 #------------------------------------------------------------#
+def scrape_target_pg(driver, page_url : str):
+    req_time_start = datetime.now().strftime("%H:%M:%S.%f")[0:-4]
+    
+    print(f'\nGetting page_url content... (LOCAL_TEST: {LOCAL_TEST})\n')
+    if not LOCAL_TEST:
+        # auto-click w/ 'immediate click'
+        driver.get(page_url) # GET html page
+                
+        # gen html obj from webdriver's html src str
+        html_cont = html.fromstring(driver.page_source)
+        html_cont_str = driver.page_source
+    else:
+        html_cont = html.fromstring(HTML_1.TEST_HTML) # from 04_test_gun_html_1.py
+        html_cont_str = HTML_1.TEST_HTML
+        driver.quit()
+    
+    # print OG html version
+    #print(f"\n\n _ html_cont (OG) _ \n{html_cont_str}")
+    #print('*** break point ***')
+    #while True: pass
 
+def init_webdriver():
+    ## Selenium: init webdrive ##
+    print(f'\nInitializing Selenium webdriver...')
+
+    # Configure Selenium options
+    options = Options()
+    options.add_argument("--headless")  # Run Chrome in headless mode
+
+    # Create a new Selenium driver & get html_content
+    return webdriver.Chrome(options=options)
+            
+def exe_pg_scrape_loop(lst_pgs: list, wait_sec : float):
+    driver = init_webdriver()
+    print(f'# pages to scrape: {len(lst_pgs)}')
+    for idx, pg_url in enumerate(lst_pgs):
+        go_time_start = get_time_now()
+        print(f'\n\npg# {idx+1}\n pg scrape start: {go_time_start}\n    url: {pg_url}')
+        scrape_target_pg(driver, pg_url)
+        print(f'\n pg scrape start: {go_time_start}\n pg scrape end:   {get_time_now()}\n    url: {pg_url}\n\n')
+        
+        # validate more pages (sleep between pgs)
+        if idx < len(lst_pgs)-1:
+            r_sec = int(random.uniform(wait_sec+WR_LOW, wait_sec+WR_HI))
+            wait_sleep(r_sec)
+        else:
+            print('** NO MORE PAGES **')
+    driver.quit()
 
 #------------------------------------------------------------#
 #   DEFAULT SUPPORT                                          #
 #------------------------------------------------------------#
-#def go_main():
-#    run_time_start = get_time_now()
-#    print(f'\n\nRUN_TIME_START: {run_time_start}')
-#    read_cli_args() # print cli args
-#    argCnt = len(sys.argv) # get arg cnt
-#
-#    # validate args
-#    if argCnt > 1:
-#        print('*** ERROR *** _ invalid args\n ... exiting\n\n')
-#        exit(1)
-#
-#    # loop through and scrape each url
-#    exe_pg_scrape_loop(LST_PG_URLS, WAIT_TIME)
-#
-#    print(f'\n\nRUN_TIME_START: {run_time_start}')
-#    print(f'RUN_TIME_END:   {get_time_now()}')
-#
-#def read_cli_args():
-#    funcname = f'<{__filename}> _ ENTER _ read_cli_args'
-#    print(f'\n{funcname}...')
-#    argCnt = len(sys.argv)
-#    print(' # of args: %i' % argCnt)
-#    print(' argv lst: %s' % str(sys.argv))
-#    for idx, val in enumerate(sys.argv):
-#        print(f' argv[{idx}]: {val}')
-#    print(f'DONE _  read_cli_args...')
-#
-#if __name__ == "__main__":
-#    go_main()
+def go_main():
+    run_time_start = get_time_now()
+    print(f'\n\nRUN_TIME_START: {run_time_start}')
+    lst_argv = read_cli_args() # print cli args
+
+    # validate args
+    if len(lst_argv) > 1:
+        print('*** ERROR *** _ invalid args\n ... exiting   {get_time_now()}\n\n')
+        exit(1)
+
+    # loop through and scrape each url
+    exe_pg_scrape_loop(LST_PG_URLS, WAIT_TIME)
+    print(f'\n\nRUN_TIME_START: {run_time_start}\nRUN_TIME_END:   {get_time_now()}')
+
+def wait_sleep(wait_sec : int, b_print=True): # sleep 'wait_sec'
+    for s in range(wait_sec, 0, -1):
+        if b_print: print('wait ', s, sep='', end='\n')
+        time.sleep(1)
+    print(f'waited... {wait_sec} sec')
+        
+def get_time_now():
+    return datetime.now().strftime("%H:%M:%S.%f")[0:-4]
+    
+def read_cli_args():
+    print(f'\nread_cli_args...\n # of args: {len(sys.argv)}\n argv lst: {str(sys.argv)}')
+    for idx, val in enumerate(sys.argv): print(f' argv[{idx}]: {val}')
+    print('read_cli_args _ DONE\n')
+    return sys.argv
+
+if __name__ == "__main__":
+    go_main()
+
+
 
 
 
