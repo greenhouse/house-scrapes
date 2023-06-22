066a40fd2178f40aff1a97b6593c263bca79afe5
house <housesellout@gmail.com>
2023-06-21 20:23:27 -0400

    :- SILK: initial silk_care_demo.py integration 	- straight copy of 05_test_silk.py 		just changed file name 		and changed _test_support.py to silk_support.py 	- added test html_1|2.py files (straight copy from _test) 	- note: note testing or cleanup done yet

---
+++ src/silk/silk_care_demo.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+__filename = 'silk_care_demo.py'
+__fname = 'silk_care_demo'
+cStrDividerExcept = '***************************************************************'
+cStrDivider = '#================================================================#'
+print('', cStrDivider, f'START _ {__filename}', cStrDivider, sep='\n')
+print(f'GO {__filename} -> starting IMPORTs and globals decleration')
+
+#------------------------------------------------------------#
+#   IMPORTS                                                  #
+#------------------------------------------------------------#
+from silk_support import * # sys, os, re, json
+from selenium import webdriver # pip install -U selenium
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+    #from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+    #from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+    #from selenium.webdriver.firefox.options import Options
+import random
+import importlib
+HTML_x = importlib.import_module('05_test_silk_html_2')
+
+#------------------------------------------------------------#
+#   GLOBALS                                                  #
+#------------------------------------------------------------#
+WAIT_TIME = 10 # sec
+WR_HI = 0 # wait range
+WR_LOW = -5 # wait range
+
+#AUTO_CLICK_WAIT = False
+DEBUG_HIDE = True
+WRITE_CSV = True
+LOCAL_TEST = False
+LST_PG_URLS = [ # GET https://www.carenity.es/foro/asma-88  (results in list of post links)
+    # example post links...
+    "https://www.carenity.es/foro/asma/tu-opinion-sobre-los-tratamientos-del-asma/tratamiento-de-las-crisis-de-asma-38588",
+    "https://www.carenity.es/foro/asma/tu-opinion-sobre-los-tratamientos-del-asma/inhalador-preventivo-33724",
+    "https://www.carenity.es/foro/asma/tu-opinion-sobre-los-tratamientos-del-asma/utilizas-ventolin-32459",
+    "https://www.carenity.es/foro/asma/tu-opinion-sobre-los-tratamientos-del-asma/es-posible-curar-el-asma-con-tratamientos-natu-32457"
+]
+LST_CSV_EXPORT = []
+
+# use 0 wait time for LOCAL_TEST
+if LOCAL_TEST: WAIT_TIME = WR_HI = WR_LOW = 0
+
+#------------------------------------------------------------#
+#   PROCEDURAL SUPPORT                                       #
+#------------------------------------------------------------#
+def scrape_target_pg(driver, page_url : str):
+    req_time_start = datetime.now().strftime("%H:%M:%S.%f")[0:-4]
+    
+    print(  f'\nGetting page_url content... w/ GLOBALS (',
+            f'\n DEBUG_HIDE: {DEBUG_HIDE},',
+            f'\n WRITE_CSV: {WRITE_CSV},',
+            f'\n LOCAL_TEST: {LOCAL_TEST},',
+            f'\n WAIT_TIME: {WAIT_TIME+WR_LOW} to {WAIT_TIME+WR_HI} sec',
+            f'\n)\n', sep='')
+            
+    if not LOCAL_TEST:
+        # auto-click w/ 'immediate click'
+        driver.get(page_url) # GET html page
+                
+        # gen html obj from webdriver's html src str
+        hc = html.fromstring(driver.page_source)
+        html_cont_str = driver.page_source
+    else:
+        hc = html.fromstring(HTML_x.TEST_HTML) # from 05_test_silk_html_1.py
+        html_cont_str = HTML_x.TEST_HTML
+        driver.quit()
+    
+    # TODO: get elements for this page_url (ie. post_url)
+    '''
+        user post elements:
+            post_view_cnt, post_support_cnt, post_comment_cnt
+            post_headline, post_usr_name, post_usr_img, post_dt, post_text
+        user comment data lists:
+            lst_comm_usr_names, lst_comm_usr_imgs, lst_comm_dts, lst_comm_texts
+        
+        user post elements  user comment data lists
+        post_view_cnt
+        post_supp_cnt
+        post_comm_cnt
+        post_headline
+        post_usr_img_url    lst_comm_usr_img_urls
+        post_un             lst_comm_uns
+        post_dt             lst_comm_dts
+        post_text           lst_comm_texts
+    '''
+    
+    ## get headline -> <h1 itemprop="headline" class="thread-title">
+    ## get headline -> "//h1[@itemprop='headline']"
+    post_headline = hc.xpath("//h1[@itemprop='headline']")[0].text_content()
+    
+    # get post_usr_img_url -> <div class="contribs-item" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+    #                           <p class="avatar-container " "="" onclick="haveToInscription(event); return false;">
+    # get post_usr_img_url -> "//div[@class='contribs-item']//p[@class='avatar-container ']//img/@src"
+    #   NOTE: this returns both post & comment user img urls
+    #           post_usr_img_url at idx = 0; comm_usr_img_urls at idx >= 1
+    post_usr_img_url = hc.xpath("//div[@class='contribs-item']//p[@class='avatar-container ']//img/@src")[0]
+    
+    ## get lst_comm_usr_img_urls -> "//div[@class='contribs-item']//p[@class='avatar-container ']//img/@src"
+    lst_comm_usr_img_urls = hc.xpath("//div[@class='contribs-item']//p[@class='avatar-container ']//img/@src")[1:]
+
+    ## get post_un & lst_comm_uns -> <p class="name" onclick="haveToInscription(event); return false;">
+    ## get post_un & lst_comm_uns -> "//p[@onclick='haveToInscription(event); return false;']"
+    #   NOTE: this returns both post & comment user names
+    #           post_un at idx = 0; comm_uns at idx >= 1
+    lst_tags = hc.xpath("//p[@onclick='haveToInscription(event); return false;']")
+    lst_uns = [tag.text_content() for tag in lst_tags if not tag.text_content().endswith('\n')]
+    post_un = lst_uns[0]
+    lst_comm_uns = lst_uns[1:]
+    
+    ## get post_dt -> <div class="date"> | "//div[@class='date']"
+    #   NOTE: this returns both post & comment datetime
+    #           post_dt at idx = 0; comm_dt at idx >= 1
+    post_dt = hc.xpath("//div[@class='date']")[0].text_content()
+    post_dt = re.sub(r"\s+", " ", post_dt.strip())
+    
+    ## get lst_comm_dts -> <div class="date"> | "//div[@class='date']"
+    #lst_comm_dts = [tag.text_content() for tag in hc.xpath("//div[@class='date']")[1:]]
+    lst_tags = hc.xpath("//div[@class='date']")[1:]
+    lst_comm_dts = [tag.text_content().strip('\n').strip() for tag in lst_tags]
+    
+    ## get post_text -> <div itemprop="text"> | "//div[@itemprop='text']"
+    post_text = hc.xpath("//div[@itemprop='text']")[0].text_content().strip()
+    
+    ## get lst_comm_texts -> <div itemprop="text"> | "//div[@itemprop='text']"
+    lst_comm_texts = [re.sub(r"\s+", " ", tag.text_content().strip()) for tag in hc.xpath("//div[@itemprop='text']")[1:]]
+    #lst_tags = hc.xpath("//div[@itemprop='text']")[1:]
+    #lst_comm_texts = [re.sub(r"\s+", " ", tag.text_content().strip()) for tag in lst_tags]
+    
+    # get post_view_cnt, post_supp_cnt, post_comm_cnt -> <ul class="thread-stats"> | "//ul[@class='thread-stats']//li"
+    lst_li_tags = hc.xpath("//ul[@class='thread-stats']//li")
+    post_view_cnt = lst_li_tags[0].text_content()
+    post_supp_cnt = lst_li_tags[1].text_content()
+    post_comm_cnt = lst_li_tags[2].text_content()
+
+    # print OG html version
+#    print(f"\n\n _ html_cont (OG) _ \n{html_cont_str}")
+#    print('*** break point ***')
+#    while True: pass
+    
+    ## PRINT SCRAPED DATA ##
+    s  = '***'
+    s0 = '\n'+s
+    d  ='#---------------------------------------------------------------------------#'
+    dd ='#===========================================================================#'
+    print(f'\n{dd}\n {page_url} \n{dd}')                                # page_url (str)
+    #print(f'\n{d}\n POST & COMMENT DATA \n{d}')
+    print(f'\n{d}\n POST DATA \n{d}')
+    print(f'{s} post_headline (text) {s}:\n    {post_headline}')        # post_headline (str)
+    print(f'{s0} post_un (text) {s}:\n    {post_un}')                   # post_un (str)
+    print(f'{s0} post_usr_img_url (text) {s}:\n    {post_usr_img_url}') # post_usr_img_url (str)
+    print(f'{s0} post_dt (text) {s}:\n    {post_dt}')                   # post_dt (str)
+    print(f'{s0} post_text (text) {s}:\n    {post_text}')               # post_text (str)
+
+    print(f'\n{d}\n COMMENT DATA LISTS \n{d}')
+    print(  f'{s} lst_comm_uns (list text x{len(lst_comm_uns)}) {s}:',
+            f'{json.dumps(lst_comm_uns, indent=4)}', sep='\n')          # lst_comm_uns (lst)
+    print(  f'{s0} lst_comm_usr_img_urls (list text x{len(lst_comm_usr_img_urls)}) {s}:',
+            f'{json.dumps(lst_comm_usr_img_urls, indent=4)}', sep='\n') # lst_comm_usr_img_urls (lst)
+    print(  f'{s0} lst_comm_dts (list text x{len(lst_comm_dts)}) {s}:',
+            f'{json.dumps(lst_comm_dts, indent=4)}', sep='\n')          # lst_comm_dts (lst)
+    print(  f'{s0} lst_comm_texts (list text x{len(lst_comm_texts)}) {s}:',
+            f'{json.dumps(lst_comm_texts, indent=4)}', sep='\n')        # lst_comm_texts (lst)
+            
+    print(f'\n{d}\n POST META DATA \n{d}')
+    print(f'{s} post_view_cnt (text) {s}:\n    {post_view_cnt}')       # post_view_cnt (str)
+    print(f'{s0} post_supp_cnt (text) {s}:\n    {post_supp_cnt}')       # post_supp_cnt (str)
+    print(f'{s0} post_comm_cnt (text) {s}:\n    {post_comm_cnt}')       # post_comm_cnt (str)
+    
+    # RETURN SCRAPED DATA DICT
+    return {'page_url':page_url, 'post_headline':post_headline, 'post_un':post_un, 'post_usr_img_url':post_usr_img_url, 'post_dt':post_dt, 'post_text':post_text, 'lst_comm_uns':lst_comm_uns, 'lst_comm_usr_img_urls':lst_comm_usr_img_urls, 'lst_comm_dts':lst_comm_dts, 'lst_comm_texts':lst_comm_texts, 'post_view_cnt':post_view_cnt, 'post_supp_cnt':post_supp_cnt, 'post_comm_cnt':post_comm_cnt}
+
+def init_webdriver():
+    ## Selenium: init webdrive ##
+    print(f'\nInitializing Selenium webdriver...')
+
+    # Configure Selenium options
+    options = Options()
+    options.add_argument("--headless")  # Run Chrome in headless mode
+
+    # Create a new Selenium driver & get html_content
+    return webdriver.Chrome(options=options)
+            
+def exe_pg_scrape_loop(lst_pgs: list, wait_sec : float):
+    driver = init_webdriver()
+    print(f'# pages to scrape: {len(lst_pgs)}')
+    for idx, pg_url in enumerate(lst_pgs):
+        go_time_start = get_time_now()
+        print(f'\n\npg# {idx+1}\n pg scrape start: {go_time_start}\n    url: {pg_url}')
+        d = scrape_target_pg(driver, pg_url)
+        print(f'\n pg scrape start: {go_time_start}\n pg scrape end:   {get_time_now()}\n    url: {pg_url}\n\n')
+        
+        # append scraped page data to global list for export
+        if d: LST_CSV_EXPORT.append(d)
+        
+        # validate more pages (sleep between pgs)
+        if idx < len(lst_pgs)-1:
+            r_sec = int(random.uniform(wait_sec+WR_LOW, wait_sec+WR_HI))
+            wait_sleep(r_sec)
+        else:
+            print('** NO MORE PAGES **')
+            
+    print(f'** QUITING WEBDRIVER & WRITING DATA TO CSV ({WRITE_CSV}) **')
+    driver.quit()
+    if WRITE_CSV: write_lst_dict_to_csv(LST_CSV_EXPORT, 'TEST_SILK_OUTPUT.csv')
+    
+
+#------------------------------------------------------------#
+#   DEFAULT SUPPORT                                          #
+#------------------------------------------------------------#
+def go_main():
+    run_time_start = get_time_now()
+    print(f'\n\nRUN_TIME_START: {run_time_start}')
+    lst_argv = read_cli_args() # print cli args
+
+    # validate args
+    if len(lst_argv) > 1:
+        print('*** ERROR *** _ invalid args\n ... exiting   {get_time_now()}\n\n')
+        exit(1)
+
+    # loop through and scrape each url
+    exe_pg_scrape_loop(LST_PG_URLS, WAIT_TIME)
+    print(f'\n\nRUN_TIME_START: {run_time_start}\nRUN_TIME_END:   {get_time_now()}')
+    
+if __name__ == "__main__":
+    go_main()
+
---
+++ src/silk/silk_support.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+__filename = '_test_support.py'
+__fname = '_test_support'
+cStrDividerExcept = '***************************************************************'
+cStrDivider = '#================================================================#'
+print('', cStrDivider, f'START _ {__filename}', cStrDivider, sep='\n')
+print(f'GO {__filename} -> starting IMPORTs and globals decleration')
+
+#------------------------------------------------------------#
+#   IMPORTS                                                  #
+#------------------------------------------------------------#
+import sys, os, re, json, time
+import openpyxl, csv
+from lxml import html
+from datetime import datetime
+
+#------------------------------------------------------------#
+#   READ / WRITE SUPPORT                                     #
+#------------------------------------------------------------#
+def read_js_file(file_path, bPrint=True):
+    print('enter _ read_js_file')
+    with open(file_path, 'r') as file:
+        print('enter with open')
+        lines = file.readlines()
+        if bPrint: print('lines:\n', *lines, sep='')
+        print(f'len(lines): {len(lines)}\n\n')
+        print('exit _ read_js_file')
+        return lines
+
+def write_lst_dict_to_csv(l : list, file_name : str ='output.csv'):
+    print('enter _ write_lst_dict_to_csv')
+    #l = [{'name': 'J', 'email': 'j@mail.com'}, {'name': 'K', 'email': 'k@mail.com'}]
+    if len(l) == 0: return
+    
+    field_names = l[0].keys() # column headers
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(field_names)
+        for d in range(0, len(l)):
+            writer.writerow(list(l[d].values()))
+        
+def write_lst_to_csv(l : list):
+    #l = ['John', 'Doe', 'john.doe@example.com']
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in l:
+            writer.writerow(l)
+        
+def write_dict_to_csv(d : dict):
+    #d = {'Name': 'John Doe', 'Email': 'john.doe@example.com'}
+    with open('output.csv', 'w', newline='') as csvfile:
+        fieldnames = d.keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(d)
+
+#------------------------------------------------------------#
+#   (UN-TESTING) READ / WRITE SUPPORT                        #
+#------------------------------------------------------------#
+#def write_lst_to_xlsx(l : list):
+#    #l = ['John', 'Doe', 'john.doe@example.com']
+#    workbook = openpyxl.Workbook()
+#    sheet = workbook.active
+#    sheet.append(l)
+#    workbook.save('output.xlsx')
+#
+#def write_dict_to_xlsx(d : dict):
+#    #d = {'Name': 'John Doe', 'Email': 'john.doe@example.com', 'Age': 30, 'City': 'New York', 'Country': 'USA'}
+#    workbook = openpyxl.Workbook()
+#    sheet = workbook.active
+#
+#    # Writing header row
+#    header_row = 1
+#    for col_num, key in enumerate(d.keys(), start=1):
+#        sheet.cell(row=header_row, column=col_num).value = key
+#
+#    # Writing data rows
+#    data_start_row = 2
+#    for row_num in range(data_start_row, data_start_row + 5):  # 5 data rows
+#        for col_num, value in enumerate(d.values(), start=1):
+#            sheet.cell(row=row_num, column=col_num).value = value
+#
+#    workbook.save('output.xlsx')
+
+#------------------------------------------------------------#
+#   DEFAULT SUPPORT                                          #
+#------------------------------------------------------------#
+def wait_sleep(wait_sec : int, b_print=True): # sleep 'wait_sec'
+    print(f'waiting... {wait_sec} sec')
+    for s in range(wait_sec, 0, -1):
+        if b_print: print('wait ', s, sep='', end='\n')
+        time.sleep(1)
+    print(f'waited... {wait_sec} sec')
+        
+def get_time_now():
+    return datetime.now().strftime("%H:%M:%S.%f")[0:-4]
+    
+def read_cli_args():
+    print(f'\nread_cli_args...\n # of args: {len(sys.argv)}\n argv lst: {str(sys.argv)}')
+    for idx, val in enumerate(sys.argv): print(f' argv[{idx}]: {val}')
+    print('read_cli_args _ DONE\n')
+    return sys.argv
